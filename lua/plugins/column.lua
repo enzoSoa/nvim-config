616881221e18611dd113840ca2c355fe0026560c
Enzo Soares <enzoayrsoares@gmail.com>
2024-10-17 17:02:07 +0200

    Adding tabs and tree

--- lua/plugins/column.lua
+++ lua/plugins/column.lua
@@ -4,7 +4,12 @@ return {
     opts = {},
     config = function()
       require("NeoColumn").setup()
-      vim.keymap.set("n", "<leader>h", "<cmd>ToggleNeoColumn<cr>", { noremap = true, silent = true })
+      vim.keymap.set(
+        "n",
+        "<leader>ct",
+        "<cmd>ToggleNeoColumn<cr>",
+        { noremap = true, silent = true }
+      )
     end,
   },
 }
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -5,6 +5,11 @@ return {
     lazy = false,
     config = function()
       require("nvim-tree").setup({
+        actions = {
+          open_file = {
+            quit_on_open = true,
+          },
+        },
         sort = {
           sorter = "case_sensitive",
         },
@@ -24,6 +29,9 @@ return {
           dotfiles = true,
         },
       })
+
+      local api = require("nvim-tree.api")
+      vim.keymap.set("n", "<leader>t", api.tree.toggle, { desc = "Nvim-tree toggle tree" })
     end,
   }
 }
